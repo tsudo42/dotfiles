714334ae09deda5fd7d5a3ec6b180108045685c5
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2021-11-27 02:20:13 +0900

    FIX wrong cp of lightline

--- centos7/install_lightline.sh
+++ centos7/install_lightline.sh
@@ -6,8 +6,8 @@ cd gitclones
 git clone https://github.com/itchyny/lightline.vim
 
 mkdir -p ~/dotfiles/vimex/pack/plugins/start/lightline
-cp -R lightline.vim/ ~/dotfiles/vimex/pack/plugins/start/lightline
+cp -R lightline.vim/* ~/dotfiles/vimex/pack/plugins/start/lightline
 
 git clone https://github.com/cocopon/iceberg.vim.git
 
-cp -R iceberg.vim/ ~/dotfiles/vimex/
+cp -R iceberg.vim/* ~/dotfiles/vimex/
